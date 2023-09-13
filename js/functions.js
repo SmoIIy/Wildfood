6cb95f7acc9fd63e1968e387c29e5538042709f2
SmoIIy <91464612+SmoIIy@users.noreply.github.com>
2023-09-13 14:06:43 +0200

    Kategori.html done

--- js/functions.js
+++ js/functions.js
@@ -3,7 +3,10 @@
 const url = "https://hnrcndxoquhelmpaqheh.supabase.co/rest/v1/dataset";
 //Variable til APIkey
 const apikey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhucmNuZHhvcXVoZWxtcGFxaGVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM4MjI5MTksImV4cCI6MjAwOTM5ODkxOX0.Kj0vgWH3imfxSBjrhKN9746mppt3v9ldhd1y2ugyfyc";
-
+//Få url fra input i hjul på forsiden
+const urlParams = new URLSearchParams(window.location.search);
+const urlEnd = urlParams.get("seasons");
+const urlEndFull = "?aarstid=eq." + urlEnd;
 //Fetch database og convert til json
 fetch(url,{
     method: "GET",
