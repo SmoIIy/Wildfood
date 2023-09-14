e94b6695601f67d80e5637952b68259c4fdff60a
SmoIIy <91464612+SmoIIy@users.noreply.github.com>
2023-09-14 16:19:40 +0200

    burgermenu done

--- js/burgermenu.js
+++ js/burgermenu.js
@@ -1,14 +1,8 @@
-const burger = document.querySelector(".burger");
-const navMenu = document.querySelector(".nav_menu");
-
-burger.addEventListener("click", () => {
-  burger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-});
-
-document.querySelectorAll("nav_link").forEach((n) =>
-  n.addEventListener("click", () => {
-    burger.classList.remove("active");
-    navMenu.classList.remove("active");
-  })
-);
+window.onload = function (){
+  const menuBtn = document.querySelector('.hamburger');
+  const mobileMenu = document.querySelector('.mobile-nav');
+  menuBtn.addEventListener('click', function(){
+      menuBtn.classList.toggle('is-active')
+      mobileMenu.classList.toggle('is-active')
+  });
+}
